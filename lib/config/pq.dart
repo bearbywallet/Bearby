d23ae8c562ac8a5cef85d64cfdf4644455cb2d97
Rinat <hicc0rus@gmail.com>
2026-05-10 23:31:36 +0900

    chore: disable kBitcoinlip44 in PostQuantumChains

--- lib/config/pq.dart
+++ lib/config/pq.dart
@@ -1,9 +1,9 @@
-import 'package:bearby/config/web3_constants.dart';
+// import 'package:bearby/config/web3_constants.dart';
 import 'package:bearby/src/rust/models/provider.dart';
 
 class PostQuantumChains {
   static const Set<int> slip44s = <int>{
-    kBitcoinlip44,
+    // kBitcoinlip44,
   };
 
   static bool contains(NetworkConfigInfo chain) =>
